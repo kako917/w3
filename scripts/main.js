ba260ee6a59946545bf3c60d90287d01243aacd1
kako917 <95006568+kako917@users.noreply.github.com>
2021-12-02 11:45:49 +0900

    change image

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/chopin.png');
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
+    myImage.setAttribute ('src','images/chopin.png');
   }
 }
 
